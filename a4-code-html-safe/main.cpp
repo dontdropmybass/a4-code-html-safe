e2f84000a45b16c6630fe5c290d0cfd03f402466
Alexander Cochrane <acochrane@almightycorp.com>
2016-11-07 13:54:07 -0400

    Initial Commit

---
+++ a4-code-html-safe/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  a4-code-html-safe
+//
+//  Created by Alexander Cochrane on 2016-11-07.
+//  Copyright © 2016 Alexander Cochrane. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
